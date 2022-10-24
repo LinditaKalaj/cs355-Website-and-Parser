640417b7bfcc7ec8c9a5159df616aabd08038fce
LinditaKalaj <91567228+LinditaKalaj@users.noreply.github.com>
2022-10-24 18:49:53 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,5 @@
 <?php
+
 $string = file_get_contents('https://www.gutenberg.org/cache/epub/68702/pg68702.html');
 function wp_strip_all_tags( $string, $remove_breaks = false ) {
     $string = preg_replace( '@<(script|style)[^>]*?>.*?</\\1>@si', '', $string );
@@ -13,3 +14,22 @@ function wp_strip_all_tags( $string, $remove_breaks = false ) {
 $string = wp_strip_all_tags($string);
 echo $string;
 ?>
+<!DOCTYPE html> 
+<html>
+<head>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<script src="https://code.jquery.com/jquery-latest.min.js"></script>
+<script> 
+        $(function(){
+            $("#globnavbar").load("navbar.html"); 
+        });
+        </script> 
+    <title>Parse</title>
+</head>
+<body>
+<div id="globnavbar"></div>
+    <h1 id = "parseResultsh1">Test</h1>
+    <div><p id = cparse>Here are the results of this parse, if you would like to view parse history 
+        click <a href="report.php">HERE</a></p></div>
+    </body>
+</html>
