18d61b4dd94ee8382e4166403301c3098f246bef
LinditaKalaj <91567228+LinditaKalaj@users.noreply.github.com>
2022-10-24 22:45:05 -0400

    Update test.php

--- test.php
+++ test.php
@@ -8,6 +8,7 @@ try {
   $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Success";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
